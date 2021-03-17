3351ab05acb42b79d0414668808b67daa928c0db
Naimah Muhammed-Yakubu <naimah.muhammed-yakubu@uwe.ac.uk>
2021-03-17 13:45:59 +0000

    Delete evaluate-news-nlp 2 2 copy directory

--- evaluate-news-nlp 2 2 copy/src/client/js/checkUrl.js
+++
@@ -1,9 +0,0 @@
-export function checkUrl(url) {
-
-    const regexr = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if (regexr.test(url)) {
-        return true;
-    } else {
-        return false;
-    }
-}
--- evaluate-news-nlp 2 2 copy/src/tests/formHandler.test.js
+++
@@ -1,7 +0,0 @@
-import { submitHandler } from '../client/js/formHandler';
-
-describe('Testing the submit functionality', () => {
-    test('submit for testing', () => {
-        expect(submitHandler).toBeDefined();
-    });
-})
